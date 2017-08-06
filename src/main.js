e1d834e3f9b39899777a03c54e2aaf80f7773d7b
王冬 <20004604@qq.com>
2017-08-06 23:40:40 +0800

    增加开发调试模式

--- src/main.js
+++ src/main.js
@@ -9,12 +9,23 @@ import filter from './plugin/filter'
 import http from './api/index'
 import copy from './plugin/copy'
 import store from './store'
+import forDevelopment from './plugin/forDevelopment'
 
 Vue.config.productionTip = false
 Vue.use(service)
 Vue.use(filter)
 Vue.use(http)
 Vue.use(copy)
+// 开发调试配置
+if (process.env.NODE_ENV === 'development') {
+  Vue.use(forDevelopment)
+} else {
+  // 非开发状态屏蔽console.log显示
+  console.log = function () {
+  }
+  console.error = function () {
+  }
+}
 
 /* eslint-disable no-new */
 new Vue({
